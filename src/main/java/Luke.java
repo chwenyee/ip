9ea03c21fe5118e1f5e29a995b188f85e7ef0ab9
Wen Yee <wenyeechan2003@gmail.com>
2025-02-04 22:56:09 +0800

    Level 2. Add, List. Luke can now store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,28 +1,48 @@
 import java.util.Scanner;
 
 public class Luke {
+    private static final String DIVIDER = "——————————————————————————————————————————————————————";
+
     public static void greeting () {
-        System.out.println("——————————————————————————————————————————————————————");
+        System.out.println(DIVIDER);
         System.out.println("Hello! I'm Luke");
         System.out.println("What can I do for you?");
-        System.out.println("——————————————————————————————————————————————————————");
+        System.out.println(DIVIDER);
+    }
+
+    public static void bye () {
+        System.out.println(DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 
     public static void main(String[] args) {
         greeting();
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         String input;
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println("——————————————————————————————————————————————————————");
-            System.out.println(input);
-            System.out.println("——————————————————————————————————————————————————————");
+            if (input.equals("list")) {
+                System.out.println(DIVIDER);
+                for (int i = 0; i < taskCount; i += 1) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(DIVIDER);
+            } else {
+                tasks[taskCount] = input;
+                System.out.println(DIVIDER);
+                System.out.println("added: " + tasks[taskCount]);
+                System.out.println(DIVIDER);
+                taskCount += 1;
+            }
             input = in.nextLine();
         }
 
-        System.out.println("——————————————————————————————————————————————————————");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("——————————————————————————————————————————————————————");
+        bye();
+
     }
 }
