d17900944237c754ad9e844bcb2ad990e71b3420
Wen Yee <wenyeechan2003@gmail.com>
2025-02-04 22:31:01 +0800

    Week 3: Level 1. Echo. Duke echos commands entered by the user, and exits when the user types the command bye.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Luke User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Luke {
-    public static void main(String[] args) {
+    public static void greeting () {
         System.out.println("——————————————————————————————————————————————————————");
         System.out.println("Hello! I'm Luke");
         System.out.println("What can I do for you?");
-        System.out.println(" ");
+        System.out.println("——————————————————————————————————————————————————————");
+    }
+
+    public static void main(String[] args) {
+        greeting();
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("——————————————————————————————————————————————————————");
+            System.out.println(input);
+            System.out.println("——————————————————————————————————————————————————————");
+            input = in.nextLine();
+        }
+
         System.out.println("——————————————————————————————————————————————————————");
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(" ");
         System.out.println("——————————————————————————————————————————————————————");
     }
 }
