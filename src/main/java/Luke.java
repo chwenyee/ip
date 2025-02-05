f93df9f636bdbafd2baadd5729d8f64eaf194a31
Wen Yee <wenyeechan2003@gmail.com>
2025-02-05 23:14:18 +0800

    Level 3. Mark as Done. Duke's new function: the ability to mark tasks as done and the ability to change the status back to not done.

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -18,7 +18,8 @@ public class Luke {
 
     public static void main(String[] args) {
         greeting();
-        String[] tasks = new String[100];
+        //String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         String input;
@@ -28,20 +29,39 @@ public class Luke {
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println(DIVIDER);
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i += 1) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + "[" + tasks[i].getStatusIcon()+ "] " + tasks[i].description);
                 }
                 System.out.println(DIVIDER);
+            } else if (input.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(input.substring(5).trim()) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsDone();
+                    System.out.println(DIVIDER);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + tasks[taskIndex].getStatusIcon()+ "] " + tasks[taskIndex].description);
+                    System.out.println(DIVIDER);
+                }
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7).trim()) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsNotDone();
+                    System.out.println(DIVIDER);
+                    System.out.println("Nice! I've marked this task as not done yet:");
+                    System.out.println("[" + tasks[taskIndex].getStatusIcon()+ "] "+ tasks[taskIndex].description);
+                    System.out.println(DIVIDER);
+                }
             } else {
-                tasks[taskCount] = input;
-                System.out.println(DIVIDER);
-                System.out.println("added: " + tasks[taskCount]);
-                System.out.println(DIVIDER);
-                taskCount += 1;
+                    tasks[taskCount] = new Task(input);
+                    System.out.println(DIVIDER);
+                    System.out.println("added: " + tasks[taskCount].description);
+                    System.out.println(DIVIDER);
+                    taskCount += 1;
             }
             input = in.nextLine();
-        }
 
+        }
         bye();
 
     }
