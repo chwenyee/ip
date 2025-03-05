8d47126c692f26468c82570a1cfaf53bfd49e832
Wen Yee <wenyeechan2003@gmail.com>
2025-03-05 22:39:08 +0800

    Luke is now able to store deadline dates as a java.time.LocalDateTime in task objects. It accept dates in a format yyyy-MM-dd HHmm and print in a format MMM dd yyyy, h:mm a.

--- src/main/java/luke/task/Deadline.java
+++ src/main/java/luke/task/Deadline.java
@@ -1,21 +1,26 @@
 package luke.task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a");
 
     public Deadline(String description, String by, boolean isDone) {
         super(description, isDone);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
 
     @Override
     public String toFileFormat() {
-        return ("D" + super.toFileFormat() + " | " + by);
+        return ("D" + super.toFileFormat() + " | " + by.format(INPUT_FORMAT));
     }
 }
--- src/main/java/luke/task/Event.java
+++ src/main/java/luke/task/Event.java
@@ -1,24 +1,30 @@
 package luke.task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a");
+
 
     public Event(String description, String from, String to, boolean isDone) {
         super(description, isDone);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, INPUT_FORMAT);
+        this.to = LocalDateTime.parse(to, INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(OUTPUT_FORMAT) + " to: " + to.format(OUTPUT_FORMAT) + ")";
     }
 
     @Override
     public String toFileFormat() {
-        return ("E" + super.toFileFormat() + " | " + from + " | " + to);
+        return ("E" + super.toFileFormat() + " | " + from.format(INPUT_FORMAT) + " | " + to.format(INPUT_FORMAT));
     }
 
 }
