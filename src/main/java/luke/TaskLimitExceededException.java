b52f10187ce9b6afede1ace4f1a50bf31c86fdb2
Wen Yee <wenyeechan2003@gmail.com>
2025-03-05 20:38:55 +0800

    Deleted an Exception class that is never used.

--- src/main/java/luke/TaskLimitExceededException.java
+++
@@ -1,9 +0,0 @@
-package luke;
-
-class TaskLimitExceededException extends LukeException {
-
-    public TaskLimitExceededException() {
-        super("You have reached the maximum number of tasks of 100!");
-    }
-
-}
